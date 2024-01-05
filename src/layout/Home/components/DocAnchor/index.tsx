da0ce43e64cf2cee1d520d43fa3f07164b8f9852
razzh <razzhavenir@163.com>
2024-01-05 21:20:41 +0800

    feat(anchor): add view width judge for mobile device

--- src/layout/Home/components/DocAnchor/index.tsx
+++ src/layout/Home/components/DocAnchor/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useCallback, useEffect } from 'react'
 import { Anchor } from '@arco-design/web-react'
 import { useI18n } from '@/hooks'
 
@@ -5,16 +6,39 @@ const AnchorLink = Anchor.Link
 
 function GuideAnchor() {
   const { t } = useI18n()
+  const [anchorCollapse, setAnchorCollapse] = useState(window.innerWidth <= 1440)
+
+  const onResize = useCallback(() => {
+    const windowWidth = window.innerWidth
+    if (windowWidth <= 1200 && !anchorCollapse) {
+      setAnchorCollapse(true)
+    }
+    if (windowWidth > 1440 && anchorCollapse) {
+      setAnchorCollapse(false)
+    }
+  }, [anchorCollapse])
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize)
+
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [onResize])
 
   return (
+    !anchorCollapse &&
     <Anchor
-      lineless boundary={80} affix={false}
+      lineless
+      boundary={80}
+      affix={false}
     >
       <AnchorLink href='#react' title={t('home.react')} />
       <AnchorLink href='#vue3' title={t('home.vue3')} />
       <AnchorLink href='#vue2' title={t('home.vue2')} />
       <AnchorLink href='#iconApi' title='Icon API' />
       <AnchorLink href='#iconProvider' title='IconProvider API' />
+      <AnchorLink href='#autoImport' title={t('home.autoImport')} />
       <AnchorLink href='#OnlineDemo' title={t('home.demo')} />
     </Anchor>
   )
