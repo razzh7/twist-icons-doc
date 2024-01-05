c95ba483de9226c936412d78c21baeb5350d40c8
razzh <razzhavenir@163.com>
2024-01-05 21:18:28 +0800

    feat: add unplugin-vue-components docs

--- src/layout/Home/components/DocContent/codes.ts
+++ src/layout/Home/components/DocContent/codes.ts
@@ -43,3 +43,22 @@ export default {
   }
 }
 </script>`
+
+export const autoImportCode = `import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { TwistIconsVueResolver } from '@twist-space/twist-icons-plugins'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        TwistIconsVueResolver({
+          version: 'vue3'
+        })
+      ]
+    })
+  ]
+})`
--- src/layout/Home/components/DocContent/index.tsx
+++ src/layout/Home/components/DocContent/index.tsx
@@ -1,11 +1,12 @@
-import { Space, Typography, Divider } from '@arco-design/web-react'
+import { Space, Typography, Divider, Link, Alert } from '@arco-design/web-react'
 import CodeDemo from '../CodeDemo'
 import Snippet from '@/components/Snippet'
 import CodeBlock from '@/components/CodeBlock'
 import IconAPI from '../IconAPI'
 import IconProviderAPI from '../IconProviderAPI'
 import { useI18n } from '@/hooks'
-import { reactCode, vue3Code, vue2Code } from './codes'
+import { reactCode, vue3Code, vue2Code, autoImportCode } from './codes'
+import { Trans } from 'react-i18next'
 import './index.less'
 
 const { Title, Paragraph } = Typography
@@ -23,7 +24,7 @@ function DocContent() {
         <Title heading={3}>
           <div className="anthor-title">
             <a id='react' href="#react">
-            #
+              #
             </a>
             {t('home.react')}
           </div>
@@ -41,7 +42,7 @@ function DocContent() {
         <Title heading={3}>
           <div className="anthor-title">
             <a id='vue3' href="#vue3">
-            #
+              #
             </a>
             {t('home.vue3')}
           </div>
@@ -49,7 +50,7 @@ function DocContent() {
         <Space direction='vertical' size='medium' style={fullWidth}>
           <Snippet>
             <CodeBlock code='npm i @twist-space/vue3-icons --save' language='bash' />
-          </Snippet >
+          </Snippet>
           <Snippet>
             <CodeBlock code={vue3Code} language='typescript' />
           </Snippet >
@@ -59,7 +60,7 @@ function DocContent() {
         <Title heading={3}>
           <div className="anthor-title">
             <a id='vue2' href="#vue2">
-            #
+              #
             </a>
             {t('home.vue2')}
           </div>
@@ -77,12 +78,12 @@ function DocContent() {
         <Title heading={3}>
           <div className="anthor-title">
             <a id='iconApi' href="#iconApi">
-            #
+              #
             </a>
-          Icon API
+            Icon API
           </div>
         </Title>
-        <Paragraph style={{ fontSize: '16px' }}>{t('api.paragraph')}</Paragraph>
+        <Paragraph className='word'>{t('api.paragraph')}</Paragraph>
         <Space direction='vertical' size='medium' style={{ width: '70%' }}>
           <IconAPI />
         </Space>
@@ -91,20 +92,49 @@ function DocContent() {
         <Title heading={3}>
           <div className="anthor-title">
             <a id='iconProvider' href="#iconProvider">
-            #
+              #
             </a>
-          IconProvider API
+            IconProvider API
           </div>
         </Title>
-        <Paragraph style={{ fontSize: '16px' }}>{t('api.iconProvider')}</Paragraph>
+        <Paragraph className='word'>{t('api.iconProvider')}</Paragraph>
         <Space direction='vertical' size='medium' style={{ width: '70%' }}>
           <IconProviderAPI />
+        </Space>
+
+        {/* Auto Import */}
+        <Title heading={3}>
+          <div className="anthor-title">
+            <a id='autoImport' href="#autoImport">
+              #
+            </a>
+            {t('home.autoImport')}
+          </div>
+        </Title>
+        <Paragraph className='word'>
+          <Trans t={t} i18nKey='autoImportDescription' count={3}>
+            If your project is Vue3/2, you can use the <Link href="https://github.com/antfu/unplugin-vue-components" target='_blank'>unplugin-vue-components</Link>
+          </Trans>
+        </Paragraph>
+        <Space direction='vertical' size='medium' style={fullWidth}>
+          <Snippet>
+            <CodeBlock code='npm i unplugin-vue-components @twist-space/twist-icons-plugins -D' language='bash' />
+          </Snippet >
+          <Snippet>
+            <CodeBlock code={autoImportCode} language='javascript' />
+          </Snippet>
+          <Alert
+            className='word'
+            style={{ fontSize: '14px' }}
+            title={t('home.tip')}
+            content={t('home.tip.description')}
+          />
         </Space>
 
         <Title heading={3}>
           <div className="anthor-title">
             <a id='OnlineDemo' href="#OnlineDemo">
-            #
+              #
             </a>
             {t('home.demo')}
           </div>
